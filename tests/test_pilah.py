3cb6c01785d11c0f68f2fd3125d899af67fc8fa2
Muhammad Radifar <m.radifar05@gmail.com>
2024-11-08 20:25:14 +0700

    test: update pilah test

--- tests/test_pilah.py
+++ tests/test_pilah.py
@@ -16,7 +16,7 @@ def test_app(mock_config):
     with (
         patch('pilah.pilah.Config', return_value=mock_config),
         patch('pilah.pilah.extract', return_value=complex_pdb_block),
-        patch('pilah.pilah.process_ligand', return_value=["ligand_mol", "ligand_mol_Hs"]),
+        patch('pilah.pilah.process_ligand', return_value=["ligand_mol", "ligand_mol_Hs", "ligand_processing_log"]),
         patch('pilah.pilah.process_protein', return_value=["protein_mol","ionization_records"]),
         patch('pilah.pilah.renumber_hydrogens'),
         patch('pilah.pilah.rename_hydrogens'),
@@ -31,7 +31,7 @@ def test_app_default_config(mock_config_default):
     with (
         patch('pilah.pilah.Config', return_value=mock_config_default),
         patch('pilah.pilah.extract', return_value=complex_pdb_block),
-        patch('pilah.pilah.process_ligand', return_value=["ligand_mol", "ligand_mol_Hs"]),
+        patch('pilah.pilah.process_ligand', return_value=["ligand_mol", "ligand_mol_Hs", "ligand_processing_log"]),
         patch('pilah.pilah.process_protein', return_value=["protein_mol","ionization_records"]),
         patch('pilah.pilah.renumber_hydrogens'),
         patch('pilah.pilah.rename_hydrogens'),
